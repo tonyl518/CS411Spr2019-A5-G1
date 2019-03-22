7f81a558f3d08a87b39c3a0e1d0b8be976816ace
Emma Howard <emmahoward@Emmas-MacBook-Pro-5.local>
2019-03-22 16:25:48 -0400

    added comments to requestExample.js

--- Web prototype/requestExample.js
+++ Web prototype/requestExample.js
@@ -3,8 +3,13 @@ const request = require('request');
 key = 'c848a51cfbmshd7d4d51371ba1bdp1c8e25jsnae2227b6fc1b'
 ingredientList = "apples,flour,sugar"
 
+// Get recipe function
 function getRecipe(ingredientList){
-	qs = {"ingredients": ingredientList};
+
+    //Query string
+    qs = {"ingredients": ingredientList};
+    
+    //Request
 	request.get({headers: {"X-RapidAPI-Key": key},
 		url:"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?",
 		qs: qs, json: true}, (err, res, body) => {
@@ -19,4 +24,5 @@ function parseResults(results){
 
 }
 
+//Run function
 getRecipe(ingredientList)

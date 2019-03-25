31e31eaf931afa4467a9e5eb7087da34c786a4b8
Emma Howard <emmahoward@dhcp-wifi-8021x-155-41-73-164.bu.edu>
2019-03-25 16:58:58 -0400

    Added a message to confirm a post request was made

--- Web prototype/routes/index.js
+++ Web prototype/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CS411Lab5Group1' });
 });
 
+/* POST ingredients list for search */
+router.post('/', (req, res) => {
+  res.send("Got your request!");
+});
+
 module.exports = router;

b40f1a264cefee22d47456b02fab4948c3154668
Emma Howard <emmahoward@dhcp-wifi-8021x-168-122-220-210.bu.edu>
2019-04-22 16:03:07 -0400

    Added user schema

---
+++ HomeChefInspiration/models/user_model.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose");
+
+var userSchema = mongoose.Schema({
+    id: String,
+    access_token: String,
+    refresh_token: String,
+    expiry_time_ms: Number, 
+    diets: [String],
+    intolerances: [String],
+    genres: [String]
+});
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;

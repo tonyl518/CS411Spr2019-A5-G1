b158214d6d446d2145cbb7903999d7cb45542a09
emmabh <emmabh97@bu.edu>
2019-04-24 12:54:48 -0400

    Added remote db

--- HomeChefInspiration/database.js
+++ HomeChefInspiration/database.js
@@ -1,13 +1,14 @@
 let mongoose = require('mongoose');
-const server = 'localhost:27017'; // REPLACE WITH YOUR DB SERVER
-const database = 'homechefinspiration';      // REPLACE WITH YOUR DB NAME
+const config = require('./config.json');
+var uri = 'mongodb+srv://' + config.db.username + ':' + config.db.pw + '@homechefinspiration-9galm.mongodb.net/homechefinspiration';
+
 class Database {
   constructor() {
     this._connect()
   }
 
 _connect() {
-     mongoose.connect(`mongodb://${server}/${database}`)
+     mongoose.connect(uri)
        .then(() => {
          console.log('Database connection successful')
        })
